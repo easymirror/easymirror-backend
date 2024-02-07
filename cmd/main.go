5e227b60a50ec38153d79437c64421d8f981f239
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-02-07 14:38:33 -0500

    Added TODOs

--- cmd/main.go
+++ cmd/main.go
@@ -3,5 +3,9 @@ package main
 import easymirrorbackend "github.com/easymirror/easymirror-backend"
 
 func main() {
+
+	// TODO initialize environment file
+	// TODO initialize database(s)
+	// TODO initialize API server
 	easymirrorbackend.Start()
 }
