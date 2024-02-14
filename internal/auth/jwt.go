26cd0396ccfb535d84fba577a0713765655b8fe7
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-02-14 13:24:14 -0500

    Updated environment variables

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -11,7 +11,8 @@ import (
 
 const (
 	// How long a JWT is valid for
-	session_length = 24 * time.Hour
+	accessTokenMaxAge  = 15 * time.Minute
+	refreshTokenMaxAge = time.Hour
 )
 
 // Generate JWT Token based in the email and in the role as input.
@@ -19,7 +20,7 @@ const (
 // Claims are pieces of info added into the tokens.
 func GenerateJWT(userID string) (string, error) {
 	// Add the signingkey and convert it to an array of bytes
-	signingKey := []byte(os.Getenv("JWT_SECRET"))
+	signingKey := []byte(os.Getenv("JWT_ACCESS_SECRET"))
 
 	// Generate a token with the HS256 as the Signign Method
 	token := jwt.New(jwt.SigningMethodHS256)
@@ -29,7 +30,7 @@ func GenerateJWT(userID string) (string, error) {
 	claims := token.Claims.(jwt.MapClaims)
 	claims["authorized"] = true
 	claims["user_id"] = userID
-	claims["exp"] = time.Now().Add(session_length).Unix()
+	claims["exp"] = time.Now().Add(accessTokenMaxAge).Unix()
 
 	// Sign the token with the signingkey defined in the step before
 	tokenStr, err := token.SignedString(signingKey)
@@ -49,7 +50,7 @@ func ValidateJWT(receivedToken string) (*jwt.Token, error) {
 			// Validate the Token and return an error if the signing token is not the proper one
 			return nil, fmt.Errorf("unexpected signing method: %v in token of type: %v", t.Header["alg"], t.Header["typ"])
 		}
-		return []byte(os.Getenv("JWT_SECRET")), nil
+		return []byte(os.Getenv("JWT_ACCESS_SECRET")), nil
 	})
 	if err != nil {
 		log.Println("Error validating JWT:", err)
