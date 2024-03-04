2e50a373f8b7bb7f2c771207bec179da8b782255
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-03-04 14:52:22 -0500

    Upload file functionality

--- internal/hosts/bunkr/upload.go
+++ internal/hosts/bunkr/upload.go
@@ -74,3 +74,109 @@ func parseGetUploadLink(resp *http.Response) (string, error) {
 	return response.URL, nil
 }
 
+func upload(ctx context.Context, albumID string, presignURI string) (string, error) {
+	// Get upload link
+	uploadLink, err := getUploadLink(ctx)
+	if err != nil {
+		return "", fmt.Errorf("getUploadLink error: %w", err)
+	}
+
+	// Get the file from the presigned URL.
+	req, _ := http.NewRequestWithContext(ctx, "GET", presignURI, nil)
+	resp1, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error getting body from presigned URL: %w", err)
+	}
+	defer resp1.Body.Close()
+
+	// We use an io.Pipe and a goroutine for writing from the file/response body
+	// and reading to the request concurrently.
+	// By doing this, we don't have to load the entire file into memory/a buffer.
+	// This also sends the file as a "transfer encoding chunked"
+	r, w := io.Pipe()
+	m := multipart.NewWriter(w)
+	go func() {
+		defer w.Close()
+		defer m.Close()
+
+		// Get the filename form the URL
+		name, err := common.FilenameFromURI(presignURI)
+		if err != nil {
+			return
+		}
+
+		part, err := m.CreateFormFile("files[]", name)
+		if err != nil {
+			return
+		}
+		if _, err = io.Copy(part, resp1.Body); err != nil {
+			return
+		}
+	}()
+
+	// Make request
+	req, _ = http.NewRequestWithContext(
+		ctx,
+		http.MethodPost,
+		uploadLink,
+		r,
+	)
+	req.Header = http.Header{
+		"sec-ch-ua":          {`"Chromium";v="122", "Not(A:Brand";v="24", "Brave";v="122"`},
+		"albumid":            {albumID},
+		"sec-ch-ua-mobile":   {"?0"},
+		"user-agent":         {userAgent},
+		"Content-Type":       {m.FormDataContentType()},
+		"accept":             {"application/json"},
+		"cache-control":      {"no-cache"},
+		"x-requested-with":   {"XMLHttpRequest"},
+		"token":              {os.Getenv("BUNKR_API_KEY")},
+		"sec-ch-ua-platform": {`"macOS"`},
+		"sec-gpc":            {"1"},
+		"accept-language":    {"en-US,en;q=0.6"},
+		"origin":             {"https://app.bunkrr.su"},
+		"sec-fetch-site":     {"cross-site"},
+		"sec-fetch-mode":     {"cors"},
+		"sec-fetch-dest":     {"empty"},
+		"referer":            {"https://app.bunkrr.su/"},
+		// "accept-encoding":    {"gzip, deflate, br"},
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("request error: %w", err)
+	}
+
+	// Parse Response
+	return parseUpload(resp)
+}
+
+// parseUpload parses the response from the `upload` function
+func parseUpload(resp *http.Response) (string, error) {
+	// Convert into JSON
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	response := &struct {
+		ErrorDescription string `json:"description"`
+		Success          bool   `json:"success"`
+		Files            []struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"files"`
+	}{}
+	if err := json.Unmarshal(body, response); err != nil {
+		return "", fmt.Errorf("unmarshal error: %w", err)
+	}
+
+	// Validate
+	if !response.Success {
+		return "", errors.New(response.ErrorDescription)
+	}
+
+	// Because we are only updating 1 file at a time, we take the first link
+	for _, file := range response.Files {
+		return file.URL, nil
+	}
+
+	// If it gets to this point, means there was no files, which is an error
+	return "", errors.New("no file links in response")
+}
--- internal/hosts/bunkr/upload_test.go
+++ internal/hosts/bunkr/upload_test.go
@@ -27,3 +27,14 @@ func TestGetUploadLink(t *testing.T) {
 
 }
 
+// go test -v -timeout 30s -run ^TestUpload$ github.com/easymirror/easymirror-backend/internal/hosts/bunkr
+func TestUpload(t *testing.T) {
+	presignURL := "https://easymirror.s3.us-east-1.amazonaws.com/48751a99-dc86-4e08-a885-ef0f75337779/mattaio.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4LVGZIJF45Z3KRFR%2F20240304%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240304T192514Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&x-id=GetObject&X-Amz-Signature=c731217155e560a1afc22cbfb25a6480e4f48936091613aa78bc66febef001d2"
+	albumID := "307806"
+
+	something, err := upload(context.Background(), albumID, presignURL)
+	if err != nil {
+		t.Fatalf("Error uploading: %v", err)
+	}
+	assert.NotEmpty(t, something)
+}
