088c1262ada3b1cf9a37f2a13835ceb3b1709a48
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-03-04 13:52:01 -0500

    Create upload.go

---
+++ internal/hosts/cyberfile/upload.go
@@ -0,0 +1,10 @@
+package cyberfile
+
+import "context"
+
+// upload upload's a given file to Cyberfile's API.
+func upload(ctx context.Context, a Account, mirrorID, presignURI, folderID string) {
+	// TODO: Prepare file
+	// TODO: Make request
+	// TODO: Parse response
+}
