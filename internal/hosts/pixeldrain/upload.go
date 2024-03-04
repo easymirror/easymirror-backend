6f6809d7eb09db54bc3eef6839a1af673bc323e2
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-03-04 10:56:53 -0500

    Upload to PixelDrain

---
+++ internal/hosts/pixeldrain/upload.go
@@ -0,0 +1,98 @@
+package pixeldrain
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+
+	"github.com/easymirror/easymirror-backend/internal/common"
+)
+
+// Upload is a wrapper function to upload to PixelDrain's API.
+func Upload(ctx context.Context, presignURI string) (string, error) {
+	return upload(ctx, presignURI)
+}
+
+// upload upload's a given file to PixelDrain's API. If the body is successfully uploaded, the ID of the upload is returned.
+func upload(ctx context.Context, presignURI string) (string, error) {
+	// Get the file from the presigned URL.
+	req, _ := http.NewRequestWithContext(ctx, "GET", presignURI, nil)
+	resp1, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error getting body from presigned URL: %w", err)
+	}
+	defer resp1.Body.Close()
+
+	// We use an io.Pipe and a goroutine for writing from the file/response body
+	// and reading to the request concurrently.
+	// By doing this, we don't have to load the entire file into memory/a buffer.
+	// This also sends the file as a "transfer encoding chunked"
+	r, w := io.Pipe()
+	m := multipart.NewWriter(w)
+	go func() {
+		defer w.Close()
+		defer m.Close()
+
+		// Get the filename form the URL
+		name, err := common.FilenameFromURI(presignURI)
+		if err != nil {
+			return
+		}
+
+		part, err := m.CreateFormFile("file", name)
+		if err != nil {
+			return
+		}
+		if _, err = io.Copy(part, resp1.Body); err != nil {
+			return
+		}
+	}()
+
+	// Upload the file to PixelDrain
+	req, _ = http.NewRequestWithContext(
+		ctx,
+		http.MethodPost,
+		baseURL+"/file",
+		r,
+	)
+	req.Header = http.Header{
+		"Content-Type":  {m.FormDataContentType()},
+		"Authorization": {authHeader()},
+	}
+
+	client := &http.Client{}
+	resp2, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error uploading to pixeldrain: %w", err)
+	}
+	defer resp2.Body.Close()
+	return parseUpload(resp2)
+}
+
+// parseUpload parses the response from the upload function
+func parseUpload(r *http.Response) (string, error) {
+	// Read the response
+	defer r.Body.Close()
+	body, _ := io.ReadAll(r.Body)
+	log.Println(string(body))
+
+	// Parse the response
+	response := &struct {
+		Success bool   `json:"success"`
+		ID      string `json:"id"`
+		ErrorID string `json:"value"`
+		Message string `json:"message"`
+	}{}
+	var err error
+	if err = json.Unmarshal(body, response); err != nil {
+		return "", fmt.Errorf("unmarshal error: %w", err)
+	}
+	if !response.Success {
+		return "", fmt.Errorf("upload error: %v - %v", response.ErrorID, response.Message)
+	}
+	return response.ID, nil
+}
