936fc157f3925688c77bc4788eb800378f2be41f
Matthew <45381190+Matthew17-21@users.noreply.github.com>
2024-03-19 21:56:23 -0400

    Create mirrorlink.go

---
+++ internal/mirrorlink/mirrorlink.go
@@ -0,0 +1,88 @@
+package mirrorlink
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/easymirror/easymirror-backend/internal/common"
+	"github.com/easymirror/easymirror-backend/internal/db"
+	"github.com/google/uuid"
+)
+
+const (
+	query_limit = 25
+)
+
+type MirrorLink struct {
+	ID         uuid.UUID `json:"id"`
+	Nickname   string    `json:"name"`
+	UploadDate time.Time `json:"upload_date"`
+	DurationMS int64     `json:"duration"`
+}
+
+// Returns a list of items a user has uploaded
+func GetUserLinks(ctx context.Context, db *db.Database, userID string, pageNum int) ([]MirrorLink, error) {
+	if db == nil {
+		return nil, errors.New("database is nil")
+	}
+
+	// Get links from database
+	tx, err := db.PostgresConn.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, fmt.Errorf("BeginTx error: %w", err)
+	}
+	rows, err := tx.Query(`
+		SELECT "id","nickname", "upload_date", "duration_ms" from mirroring_links
+		WHERE created_by_id=($1)
+		LIMIT ($2)
+		OFFSET ($3)
+	`, userID, query_limit, common.GetPageOffset(query_limit, pageNum))
+	if err != nil {
+		return nil, fmt.Errorf("query error: %w", err)
+	}
+
+	// Parse links
+	links := []MirrorLink{}
+	defer rows.Close()
+	for rows.Next() {
+
+		// Scan the results into the appropriate variables.
+		// We want to use temporary sql null variables since some values can be null.
+		var link MirrorLink
+		var tempName sql.NullString
+		var tempDate sql.NullTime
+		var tempDuration sql.NullInt64
+		if err := rows.Scan(&link.ID, &tempName, &tempDate, &tempDuration); err != nil {
+			log.Println("Error scanning row:", err)
+			continue
+		}
+
+		// If there are no errors, update the temp values and append to array
+		link.UploadDate = tempDate.Time
+		link.DurationMS = tempDuration.Int64
+		link.Nickname = tempName.String
+		links = append(links, link)
+	}
+
+	// Return
+	return links, nil
+}
+
+// UpdateName update's the name of a given mirror link
+func UpdateName(ctx context.Context, db *db.Database, mirrorID, name string) error {
+	return nil
+}
+
+// Delete delete's a given mirror link
+func Delete(ctx context.Context, db *db.Database, mirrorID, name string) error {
+	return nil
+}
+
+// GetHostLinks returns the host links from a mirror link
+func GetHostLinks(ctx context.Context, db *db.Database, mirrorID string) error {
+	return nil
+}
